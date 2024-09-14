7eaffbd93929782bbfb31de872d3e34e9991dd02
A-fauzi <akhmadfauzi0101@gmail.com>
2024-09-15 03:29:28 +0700

    update resume

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -67,70 +67,56 @@ export const DATA = {
   },
 
   work: [
-    {
-      company: "Atomic Finance",
-      href: "https://atomic.finance",
-      badges: [],
-      location: "Remote",
-      title: "Bitcoin Protocol Engineer",
-      logoUrl: "/atomic.png",
-      start: "May 2021",
-      end: "Oct 2022",
-      description:
-        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
-    },
-  ],
-  education: [
-    {
-      school: "Buildspace",
-      href: "https://buildspace.so",
-      degree: "s3, s4, sf1, s5",
-      logoUrl: "/buildspace.jpg",
-      start: "2023",
-      end: "2024",
-    },
-  ],
-  projects: [
-    {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
-      active: true,
-      description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-  ],
-  hackathons: [
-    {
-      title: "Hack Western 5",
-      dates: "November 23rd - 25th, 2018",
-      location: "London, Ontario",
-      description:
-        "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
-      image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
-      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
-      links: [],
-    },
-  ],
+  {
+    company: "",
+    href: "",
+    badges: [],
+    location: "",
+    title: "",
+    logoUrl: "",
+    start: "",
+    end: "",
+    description: "",
+  },
+],
+education: [
+  {
+    school: "",
+    href: "",
+    degree: "",
+    logoUrl: "",
+    start: "",
+    end: "",
+  },
+],
+projects: [
+  {
+    title: "",
+    href: "",
+    dates: "",
+    active: false,
+    description: "",
+    technologies: [],
+    links: [
+      {
+        type: "",
+        href: "",
+        icon: null,
+      },
+    ],
+    image: "",
+    video: "",
+  },
+],
+hackathons: [
+  {
+    title: "",
+    dates: "",
+    location: "",
+    description: "",
+    image: "",
+    mlh: "",
+    links: [],
+  },
+],
 } as const;
